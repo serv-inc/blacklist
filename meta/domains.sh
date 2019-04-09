c7cc9bdf3c8ba1f9cf0840917199b6e515199f29
serv-inc <serv-inc@users.noreply.github.com>
2019-04-09 14:19:48 +0200

    refactor: domains.sh needed unzipped majestic file
    - which was > 50MB

--- meta/domains.sh
+++ meta/domains.sh
@@ -6,6 +6,6 @@ ALEXA=alexa.top-1m.csv.zip
 # from http://s3-us-west-1.amazonaws.com/umbrella-static/top-1m.csv.zip
 OBSD=obsd.top-1m.csv.zip
 # from http://downloads.majestic.com/majestic_million.csv
-MAJESTIC=majestic_million.csv 
+MAJESTIC=majestic_million.zip
 
-(funzip $ALEXA | cut -d, -f 2; funzip $OBSD | cut -d, -f 2 | tr -d '\r'; cut -d',' -f7 $MAJESTIC  | tail -n +2) | sort | uniq
+(funzip $ALEXA | cut -d, -f 2; funzip $OBSD | cut -d, -f 2 | tr -d '\r'; funzip $MAJESTIC | cut -d',' -f7 | tail -n +2) | sort | uniq
