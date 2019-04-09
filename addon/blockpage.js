8c447c716f4dd7475d7e61c4a3baf772ebcd6f5e
serv-inc <serv-inc@users.noreply.github.com>
2019-04-09 13:59:20 +0200

    bug: was less linted

--- addon/blockpage.js
+++ addon/blockpage.js
@@ -1,6 +1,6 @@
 "use strict";
 /* jshint esversion: 6, strict: global */
-/* globals document */
+/* globals document, navigator */
 // licensed under the MPL 2.0 by (github.com/serv-inc)
 
 document.querySelector("#page").innerText = decodeURIComponent(document.location.search.slice(1));
